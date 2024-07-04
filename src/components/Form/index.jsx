a57f2ce3508efb1a75183a746ecb8dbce8977a5b
Murillo Silva <murillobr22@gmail.com>
2024-07-04 12:24:06 -0300

    update route form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -42,7 +42,7 @@ export function Form() {
         <button type="submit">
           Enviar Mensagem <img src={paperPlane} alt="paperPlane" />
         </button>
-        <input type="hidden" name="redirectTo" value="https://example.com/contact/success"/>
+        <input type="hidden" name="redirectTo" value="https://murilloressineti.com.br/"/>
         <input type="hidden" name="accessKey" value="6cd41307-a7c3-41c4-bd87-230cae59d44e"/>
       </fieldset>
     </Container>
